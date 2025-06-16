4e7c7df58444729b1c12baa8302e678f5a79ba7f
May-CR <guachaminemilia@gmail.com>
2025-06-16 16:28:31 -0500

    Configuración de ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,36 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  // Reglas para código de Node.js
+  {
+    files: ["/*.js"],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: 2021,
+    },
+    plugins: { js },
+    extends: ["js/recommended"]
+  },
+
+  // Reglas específicas para JS en el navegador (como /public/*.js)
+  {
+    files: ["public//*.js"],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 2021,
+    }
+  },
+
+  // Reglas específicas para pruebas con Jest
+  {
+    files: ["tests//*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      },
+      ecmaVersion: 2021,
+    }
+  }
 ]);
